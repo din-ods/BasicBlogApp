876f831ef5d9094d51d07448cee00cc16a1095c8
din-ods <webdeveloper0996@gmail.com>
2026-07-26 23:53:30 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import authservice from "./appwrite/auth"
+import authservice from "./appwrite/auth";
 import "./App.css";
+import { login, logout } from "./store/authSlice"
+import { Footer, Header } from './components/import.js'
+import {Outlet} from 'react-router-dom'
 
 function App() {
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-    authservice.getCurrentUser()
-    .then((userData)=>{
-      if(userData){
-        dispatch(login({userData}))
-      } else{
-        dispatch(logout())
-      }
-    })
-    .finally(()=> setLoading(false))
-  },[])
+  useEffect(() => {
+    authservice
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-
-
-return ! loading ? <div className): (null)
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div
+      className='w-fit block'>
+        <Header/>
+        <main>
+        TODO: {Outlet}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) :null
+  
 }
 
 export default App;
--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -23,6 +23,6 @@ const authSlice = createSlice({
 })
 
 
-export const { login, logout } = authSLice.action;
+export const { login, logout } = authSlice.actions;
 
 export default authSlice.reducer;
