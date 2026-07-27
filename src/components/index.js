4f5053f70ad3b54484ad9c4404a0b40450ad651d
din-ods <webdeveloper0996@gmail.com>
2026-07-27 23:28:13 +0530

    header and footer pages done

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/container";
+import logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn
+}
