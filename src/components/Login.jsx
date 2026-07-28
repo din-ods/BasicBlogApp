980d171d5ae4ab6896f9e90da031c958cac56731
din-ods <webdeveloper0996@gmail.com>
2026-07-28 22:37:53 +0530

    done

---
+++ src/components/Login.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import {login as authLogin} from '../store/authSlice'
+import {Button, Input, Logo} from "./index"
+import {useDispatch} from "react-redux"
+import {authService} from "../appwrite/auth"
+import {useForm} from "react-hook-form"
+
+
+function Login(){
+const navigate = useNavigate()
+const dispatch= useDispatch()
+const {register, handleSubmit}= useDispatch()
+
+
+  return(
+    <div>Login</div>
+  )
+}
+export default Login
